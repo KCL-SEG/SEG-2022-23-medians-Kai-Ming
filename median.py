d56f882f5a4406952ebb71786668815fe81009ed
Kai Ming Tey <kai.tey@kcl.ac.uk>
2022-10-02 23:26:17 +0100

    Finished median calculator

--- median.py
+++ median.py
@@ -5,8 +5,17 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
+        listLen = len(numbers)
+        print(listLen)
+        if(listLen % 2 == 0):
+            median = (numbers[int(listLen / 2)] + numbers[int(listLen / 2 - 1)]) / 2
+            print(median)
+        else: 
+            median = numbers[int(listLen / 2)]
+            print(median)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+#print(numbers)
